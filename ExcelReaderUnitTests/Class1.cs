d3eaa4cc7f4317e23352716d52b13e150bccb48a
Myroslava Konko <myroslava_konko@epam.com>
2018-10-05 13:02:41 +0300

    add draft unit test

--- ExcelReaderUnitTests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ExcelReaderUnitTests
-{
-    public class Class1
-    {
-    }
-}
---
+++ ExcelReaderUnitTests/ExcelReaderTests.cs
@@ -0,0 +1,23 @@
+﻿using ExcelReader.ReportExport;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace ExcelReaderUnitTests
+{
+    [TestFixture]
+    public class ExcelReaderTest
+    {
+        [Test]
+        public void VerifyThatTxtFileIsPresentInDirectoryAfterSaving()
+        {
+            var reportSaver = new BookstoreReportExpotToTxt();
+            var testData = new List<string> { "tes1", "test2" };
+
+            reportSaver.SaveBookReportToFile(testData);
+
+            List<string> txtFilesInDirectory = new List<string>();
+
+            Assert.IsNotEmpty(txtFilesInDirectory, "No txt file was found after report saving in the directory!");
+        }
+    }
+}
